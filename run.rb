cd52c56f07eee20bc0d5c342a59d2da1b377c669
magic-onion <alexander.m.truong@gmail.com>
2019-11-08 13:50:44 -0600

    still very close

--- run.rb
+++ run.rb
@@ -3,30 +3,3 @@ require_relative './App'
 
 app = App.new
 app.launch
-
-
-# query format
-# a mehtod to make the hash
-# a method that takes in the has and displays results
-
-# a test???
-
-
-# Type in a query and display a list of 5 books matching that query.
-  #display results nicely
-
-# Each item in the list should include the book's author, title, and publishing company.
-#yup
-
-# A user should be able to select a book from the five displayed to save to a “Reading List”
-# View a “Reading List” with all the books the user has selected from their queries -- this is a local reading list and not tied to Google Books’s account features.
-# Reading list displays five at a time with next and back commands?
-# Read me
-# Short welcome prompts
-# next/back
-# Error handling
-
-#write a method that takes in an array of output, allows the user to select one to add  to reading list
-
-
-# two cases values are nil and authors is an array
