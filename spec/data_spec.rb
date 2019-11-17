31fbe23675ff9692e46a15b04848a8130f533d8d
magic-onion <alexander.m.truong@gmail.com>
2019-11-17 17:26:37 -0600

    another one

--- spec/data_spec.rb
+++ spec/data_spec.rb
@@ -5,6 +5,7 @@ RSpec.describe DataHandler do
 
   describe "sanitize_data" do
     it "returns an array" do
+
       app = App.new
       test_array = app.sanitize_data(app.api_searcher("harry potter"))
       expect(test_array).to be_instance_of(Array)
@@ -14,9 +15,20 @@ RSpec.describe DataHandler do
 
   describe "sanitize_data" do
     it "returns a has with a key of :author" do
+
       app = App.new
       test_array = app.sanitize_data(app.api_searcher("freakonomics"))
       expect(test_array[0]).to have_key(:author)
+
+    end
+  end
+
+  describe "sanitize_data" do
+    it "handles multiple authors" do
+
+      app = App.new
+      test_array = app.sanitize_data(app.api_searcher("freakonomics"))
+      expect(test_array[0][:author]).to include(",")
 
     end
   end
