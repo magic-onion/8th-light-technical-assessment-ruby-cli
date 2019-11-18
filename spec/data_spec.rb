81cda34dabd871188b59171ccc88420d8bd8c747
magic-onion <alexander.m.truong@gmail.com>
2019-11-18 12:23:57 -0600

    cleaned up some redundancies

--- spec/data_spec.rb
+++ spec/data_spec.rb
@@ -3,11 +3,14 @@ require_relative '../environment.rb'
 
 RSpec.describe DataHandler do
 
+  before do
+    @app= App.new
+  end
+
   describe "sanitize_data" do
     it "returns an array of hashes" do
 
-      app = App.new
-      test_array = app.sanitize_data(app.api_searcher("harry potter"))
+      test_array = @app.sanitize_data(@app.api_searcher("harry potter"))
       expect(test_array).to be_instance_of(Array)
       expect(test_array[0]).to be_instance_of(Hash)
 
@@ -17,8 +20,7 @@ RSpec.describe DataHandler do
   describe "sanitize_data" do
     it "returns an array of hashes, each hash has a key of :author" do
 
-      app = App.new
-      test_array = app.sanitize_data(app.api_searcher("harry potter"))
+      test_array = @app.sanitize_data(@app.api_searcher("harry potter"))
       expect(test_array[0]).to have_key(:author)
 
     end
@@ -27,8 +29,7 @@ RSpec.describe DataHandler do
   describe "sanitize_data" do
     it "handles multiple authors" do
 
-      app = App.new
-      test_array = app.sanitize_data(app.api_searcher("freakonomics"))
+      test_array = @app.sanitize_data(@app.api_searcher("freakonomics"))
       expect(test_array[0][:author]).to include(",")
 
     end
@@ -36,10 +37,10 @@ RSpec.describe DataHandler do
 
   describe "handle_authors" do
     it "returns a hash with a key of :author and value of some string" do
-      app = App.new
-      response = app.api_searcher("freakonomics")
+
+      response = @app.api_searcher("freakonomics")
       hash = {}
-      return_book = app.handle_authors(response["items"][0]["volumeInfo"], hash)
+      return_book = @app.handle_authors(response["items"][0]["volumeInfo"], hash)
       expect(return_book[:author].length).to be > 0
     end
   end
